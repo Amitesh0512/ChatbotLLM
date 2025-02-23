dd6d565a307d13a9b549419bdb6faf40c4668767
Amitesh Surwar <amitesh.surwar1@gmail.com>
2025-02-23 16:42:50 +0530

    Changes For GitIgnore and Program File

--- Program.cs
+++ Program.cs
@@ -12,32 +12,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
+// Add Authentication Services
 var jwtKey = builder.Configuration["JwtSettings:Key"];
 var key = Encoding.ASCII.GetBytes(jwtKey);
 
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
-{
-options.RequireHttpsMetadata = false;
-options.SaveToken = true;
-options.TokenValidationParameters = new TokenValidationParameters
-{
-    ValidateIssuerSigningKey = true,
-    IssuerSigningKey = new SymmetricSecurityKey(key),
-    ValidateIssuer = true,
-    ValidateAudience = true,
-    ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-    ValidAudience = builder.Configuration["JwtSettings:Audience"]
-};
-});
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"]
+        };
+   });
 
 builder.Services.AddScoped<JwtService>();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
